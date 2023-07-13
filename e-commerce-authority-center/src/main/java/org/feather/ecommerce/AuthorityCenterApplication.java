a6ee918266f247239615a6ebe81e2ce138374254
feather <Brian608@163.com>
2023-07-13 22:27:26 +0800

    鉴权中心

---
+++ e-commerce-authority-center/src/main/java/org/feather/ecommerce/AuthorityCenterApplication.java
@@ -0,0 +1,22 @@
+package org.feather.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @projectName: e-commerce-springcloud
+ * @package: org.feather.ecommerce
+ * @className: AuthorityCenterApplication
+ * @author: feather
+ * @description: TODO
+ * @since: 2023-07-13 22:25
+ * @version: 1.0
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuthorityCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityCenterApplication.class,args);
+    }
+}
